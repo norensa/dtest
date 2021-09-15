d35e29b55cb9edf73ab42144f113f6220941fe86
Noah Orensa <noahorensa@gmail.com>
2021-09-15 11:09:12 -0600

    prevent spurious failures in test_performance_test

--- test/test_performance_test.cpp
+++ test/test_performance_test.cpp
@@ -6,14 +6,14 @@ perf("performance-test", "pass")
     for (int i = 0; i < 1000000; ++i);
 })
 .baseline([] {
-    for (int i = 0; i < 2000000; ++i);
+    for (int i = 0; i < 8000000; ++i);
 });
 
 perf("performance-test", "too-slow")
 .dependsOn("unit-test")
 .expect(Status::TOO_SLOW)
 .body([] {
-    for (int i = 0; i < 2000000; ++i);
+    for (int i = 0; i < 8000000; ++i);
 })
 .baseline([] {
     for (int i = 0; i < 1000000; ++i);
